4569959676dc3dd5d40e93602ac4edb363848f5a
ozXoz <honourkorkmaz@gmail.com>
2023-09-22 21:11:23 -0400

    created db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose');
+const cors = require('cors'); // Add this line
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Enable credentials (cookies, authorization headers, etc)
+  }));
+
+  mongoose.connect("mongodb+srv://test99:Azr2010q+@cluster0.lgsng1z.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
